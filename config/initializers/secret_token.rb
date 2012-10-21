7c7d17d7a6c0c7270719f69605244ae76cd6ac93
Felix Rabe <public@felixrabe.net>
2012-10-21 18:09:13 +0200

    rails new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LessMoney::Application.config.secret_token = 'a7a5e98b31ed04ab523a0b946c4ed261377a55e39b6561bfb91fa94a1319b58c8502ee900b58e2ad8ab3b528fa4b547a424ad0bcd3d7ddecf3715a71b738e17c'
